9217ec39ab087c4490bc74951b7908588c2f83cd
= <=>
2025-02-20 23:53:37 +0000

    25w08

--- src/modules/momentVault/routes/index.ts
+++ src/modules/momentVault/routes/index.ts
@@ -7,6 +7,8 @@ import { BaseResponse } from "../../../interfaces/base_response";
 import fs from "fs";
 import { IMomentVaultEntry } from "../../../interfaces/moment_vault_interfaces";
 import ffmpeg from "fluent-ffmpeg";
+import { checkExistence } from "../../../utils/PBRecordValidator";
+import { fetchAI } from "../../../utils/fetchAI";
 
 const router = express.Router();
 
@@ -85,6 +87,20 @@ router.post(
   }
 );
 
+router.delete("/entries/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!(await checkExistence(req, res, "moment_vault_entries", id))) {
+    return;
+  }
+
+  const { pb } = req;
+
+  await pb.collection("moment_vault_entries").delete(id);
+
+  successWithBaseResponse(res, undefined, 204);
+});
+
 router.post(
   "/transcribe",
   singleUploadMiddleware,
@@ -118,7 +134,29 @@ router.post(
 
     fs.unlinkSync(file.path);
 
-    successWithBaseResponse(res, transcription.text);
+    const response = await fetchAI({
+      provider: "openai",
+      apiKey,
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful assistant in adding punctuations to voice transcription. Given a voice transcription, your task is to add punctuations to the transcription without changing any words.",
+        },
+        {
+          role: "user",
+          content: transcription.text,
+        },
+      ],
+    });
+
+    if (!response) {
+      clientError(res, "Failed to add punctuations to transcription");
+      return;
+    }
+
+    successWithBaseResponse(res, response);
   }
 );
 
