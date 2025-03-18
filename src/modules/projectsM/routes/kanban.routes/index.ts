3f98acef0874e11c479cef59b600d43b0f6ce4bc
= <=>
2025-03-18 11:36:55 +0800

    25w12

--- src/modules/projectsM/routes/kanban.routes/index.ts
+++ src/modules/projectsM/routes/kanban.routes/index.ts
@@ -4,5 +4,6 @@ import columnsRoutes from "./routes/columns.routes";
 const router = express.Router();
 
 router.use("/columns", columnsRoutes);
+router.use("/entries", columnsRoutes);
 
 export default router;
